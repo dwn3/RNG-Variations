beb1a2d55ad60917cf8eabe028d0bc1218109f6e
David Nastase <davidwnastase@gmail.com>
2024-06-28 15:21:52 -0400

    initial commit
    copying over really ugly work while getting back up to speed with c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var rand = new Random(17);
+        int[] ints = new int[100000];
+        for(int a = 0; a < 100000; a++){
+            ints[a] = (rand.Next(100) + rand.Next(100) )/2 +1;
+        }
+
+        using(StreamWriter output = new StreamWriter("rng17.txt")){
+            for(int a = 0; a < 100000; a++)
+                output.WriteLine(ints[a]);
+        }
+    }
+}
